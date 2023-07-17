c35879cc475bd10912c45270f73e4e2bd8c52163
John Wass <jwass3@gmail.com>
2023-07-16 23:13:29 -0400

    demo the thing

---
+++ examples/demo.rs
@@ -0,0 +1,63 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+
+use defmt::info;
+use embassy_executor;
+use embassy_executor::Spawner;
+use embassy_nrf::gpio::{Level, Output, OutputDrive};
+use embassy_time::{Duration, Timer};
+
+use {defmt_rtt as _, panic_probe as _};
+use example_rust_embassy::T;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_nrf::init(Default::default());
+    info!("Starting!");
+
+    let mut t0 = T::new(Output::new(p.P1_01, Level::High, OutputDrive::Standard));
+    let mut t1 = T::new(Output::new(p.P1_02, Level::High, OutputDrive::Standard));
+    let mut t2 = T::new(Output::new(p.P1_08, Level::High, OutputDrive::Standard));
+    let mut t3 = T::new(Output::new(p.P1_10, Level::High, OutputDrive::Standard));
+
+    loop {
+        t0.activate_for(3).await;
+        t1.activate_for(3).await;
+        t2.activate_for(3).await;
+        t3.activate_for(3).await;
+
+        //
+
+        futures::join!(t0.activate_for(3), t3.activate_for(3));
+
+        //
+
+        futures::join!(t1.activate_for(3), t2.activate_for(3));
+
+        //
+
+        let x = 3;
+        for i in 0..x {
+            t0.activate_for(x - i).await;
+            t1.activate_for(x - i).await;
+            t2.activate_for(x - i).await;
+            t3.activate_for(x - i).await;
+        }
+
+        //
+
+        futures::join!(t0.activate_for(3), t1.activate_for(1), t2.activate_for(1), t3.activate_for(3));
+        futures::join!(t0.activate_for(4), t1.activate_for(3), t2.activate_for(2), t3.activate_for(1));
+        futures::join!(t0.activate_for(1), t1.activate_for(2), t2.activate_for(3), t3.activate_for(4));
+
+        //
+
+        futures::join!(t0.activate_after(1), t1.activate_after(2), t2.activate_after(3), t3.activate_after(4));
+        futures::join!(t0.deactivate_after(1), t1.deactivate_after(2), t2.deactivate_after(3), t3.deactivate_after(4));
+
+        //
+
+        Timer::after(Duration::from_secs(5)).await;
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,39 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![no_std]
+
+
+use embassy_nrf::gpio::Output;
+use embassy_time::{Duration, Timer};
+
+pub struct T<'a, P: embassy_nrf::gpio::Pin> {
+    p: Output<'a, P>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<'a, P: embassy_nrf::gpio::Pin> T<'a, P> {
+    pub fn new(p: Output<'a, P>) -> Self {
+        Self { p }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub fn activate(&mut self) {
+        self.p.set_low();
+    }
+
+    pub fn deactivate(&mut self) {
+        self.p.set_high();
+    }
+
+    pub async fn activate_for(&mut self, secs: u64) {
+        self.activate();
+        Timer::after(Duration::from_secs(secs)).await;
+        self.deactivate()
+    }
+
+    pub async fn activate_after(&mut self, secs: u64) {
+        Timer::after(Duration::from_secs(secs)).await;
+        self.activate();
+    }
+
+    pub async fn deactivate_after(&mut self, secs: u64) {
+        Timer::after(Duration::from_secs(secs)).await;
+        self.deactivate();
     }
 }
