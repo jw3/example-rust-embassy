57adf399318bb82362085508c2c9dab5d04d2ead
John Wass <jwass3@gmail.com>
2023-07-22 13:37:59 -0400

    adc and select

--- examples/demo.rs
+++ examples/demo.rs
@@ -5,66 +5,116 @@
 use defmt::{debug, info};
 use embassy_executor;
 use embassy_executor::Spawner;
+use embassy_nrf::bind_interrupts;
 use embassy_nrf::gpio::{Level, Output, OutputDrive};
+use embassy_nrf::saadc::{ChannelConfig, Config, Resistor, Saadc};
+use embassy_sync::channel::Channel;
+use embassy_sync::pubsub::PubSubBehavior;
 use embassy_time::{Duration, Timer};
 
-use {defmt_rtt as _, panic_probe as _};
-use example_rust_embassy::T;
+use example_rust_embassy::{OutputChannel, InputBus, OutputM, T, InputM};
+
+static INPUTS: InputBus =  InputBus::new();
+static OUTPUTS: OutputChannel = Channel::new();
+
+bind_interrupts!(struct Irqs {
+    SAADC => embassy_nrf::saadc::InterruptHandler;
+});
+
+#[embassy_executor::task]
+async fn s() {
+    loop {
+        match OUTPUTS.recv().await {
+            OutputM::T => info!("timeout"),
+            OutputM::M => info!("------ m -------"),
+            OutputM::H => info!("++++++ h +++++++"),
+        }
+    }
+}
+
+fn h(v: &i16) -> bool {
+    *v > 4000
+}
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let p = embassy_nrf::init(Default::default());
+async fn main(spawner: Spawner) {
+    let mut p = embassy_nrf::init(Default::default());
     info!("Starting!");
 
-    let mut t0 = T::new("d2", Output::new(p.P1_01, Level::High, OutputDrive::Standard));
-    let mut t1 = T::new("d3", Output::new(p.P1_02, Level::High, OutputDrive::Standard));
-    let mut t2 = T::new("d4", Output::new(p.P1_08, Level::High, OutputDrive::Standard));
-    let mut t3 = T::new("d5", Output::new(p.P1_10, Level::High, OutputDrive::Standard));
+    let mut t0 = T::new("d2", INPUTS.subscriber().unwrap(), &OUTPUTS, Output::new(p.P1_01, Level::High, OutputDrive::Standard));
+    let mut t1 = T::new("d3", INPUTS.subscriber().unwrap(), &OUTPUTS, Output::new(p.P1_02, Level::High, OutputDrive::Standard));
+    let mut t2 = T::new("d4", INPUTS.subscriber().unwrap(), &OUTPUTS, Output::new(p.P1_08, Level::High, OutputDrive::Standard));
+    let mut t3 = T::new("d5", INPUTS.subscriber().unwrap(), &OUTPUTS, Output::new(p.P1_10, Level::High, OutputDrive::Standard));
 
-    loop {
-        debug!("A");
-        t0.activate_for(3).await;
-        t1.activate_for(3).await;
-        t2.activate_for(3).await;
-        t3.activate_for(3).await;
-
-        //
-
-        debug!("B");
-        futures::join!(async { t0.activate_for(3).await }, async { t3.activate_for(3).await });
-
-        //
-
-        debug!("C");
-        futures::join!(async { t1.activate_for(3).await }, async { t2.activate_for(3).await });
-
-        //
-
-        debug!("D");
-        let x = 3;
-        for i in 0..x {
-            t0.activate_for(x - i).await;
-            t1.activate_for(x - i).await;
-            t2.activate_for(x - i).await;
-            t3.activate_for(x - i).await;
-        }
-
-        //
-
-        debug!("E");
-        futures::join!(async { t0.activate_for(3).await }, async { t1.activate_for(1).await }, async { t2.activate_for(1).await }, async { t3.activate_for(3).await });
-        futures::join!(async { t0.activate_for(4).await }, async { t1.activate_for(3).await }, async { t2.activate_for(2).await }, async { t3.activate_for(1).await });
-        futures::join!(async { t0.activate_for(1).await }, async { t1.activate_for(2).await }, async { t2.activate_for(3).await }, async { t3.activate_for(4).await });
-
-        //
-
-        debug!("F");
-        futures::join!(async { t0.activate_after(1).await }, async { t1.activate_after(2).await }, async { t2.activate_after(3).await }, async { t3.activate_after(4).await });
-        futures::join!(async { t0.deactivate_after(1).await }, async { t1.deactivate_after(2).await }, async { t2.deactivate_after(3).await }, async { t3.deactivate_after(4).await });
-
-        //
-
-        debug!("Completed");
-        Timer::after(Duration::from_secs(5)).await;
+    let config = Config::default();
+    let mut pins = [
+        ChannelConfig::single_ended(&mut p.P0_28),
+        ChannelConfig::single_ended(&mut p.P0_29),
+        ChannelConfig::single_ended(&mut p.P0_30),
+        ChannelConfig::single_ended(&mut p.P0_31)];
+    for p in &mut pins {
+        p.resistor = Resistor::PULLDOWN;
     }
+
+    let mut saadc = Saadc::new(p.SAADC, Irqs, config, pins);
+
+    spawner.spawn(s()).unwrap();
+
+    let chk = async {
+        loop {
+            let mut buf = [0; 4];
+            saadc.sample(&mut buf).await;
+            for (i, b) in buf.iter().enumerate() {
+                if h(b) {
+                    debug!("h {} -- {}", i, b);
+                    INPUTS.publish_immediate(InputM::H(i));
+                }
+            }
+        }
+    };
+
+    let script = async {
+        loop {
+            t0.activate_for(3).await;
+            t1.activate_for(3).await;
+            t2.activate_for(3).await;
+            t3.activate_for(3).await;
+
+            //
+
+            futures::join!(async { t0.activate_for(3).await }, async { t3.activate_for(3).await });
+
+            //
+
+            futures::join!(async { t1.activate_for(3).await }, async { t2.activate_for(3).await });
+
+            //
+
+            let x = 3;
+            for i in 0..x {
+                t0.activate_for(x - i).await;
+                t1.activate_for(x - i).await;
+                t2.activate_for(x - i).await;
+                t3.activate_for(x - i).await;
+            }
+
+            //
+
+            futures::join!(async { t0.activate_for(3).await }, async { t1.activate_for(1).await }, async { t2.activate_for(1).await }, async { t3.activate_for(3).await });
+            futures::join!(async { t0.activate_for(4).await }, async { t1.activate_for(3).await }, async { t2.activate_for(2).await }, async { t3.activate_for(1).await });
+            futures::join!(async { t0.activate_for(1).await }, async { t1.activate_for(2).await }, async { t2.activate_for(3).await }, async { t3.activate_for(4).await });
+
+            //
+
+            futures::join!(async { t0.activate_after(1).await }, async { t1.activate_after(2).await }, async { t2.activate_after(3).await }, async { t3.activate_after(4).await });
+            futures::join!(async { t0.deactivate_after(1).await }, async { t1.deactivate_after(2).await }, async { t2.deactivate_after(3).await }, async { t3.deactivate_after(4).await });
+
+            //
+
+            debug!("Restarting after 5 seconds");
+            Timer::after(Duration::from_secs(5)).await;
+        }
+    };
+
+    futures::join!(chk, script);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,45 @@
 #![no_std]
 
-
 use defmt::debug;
+use embassy_futures::select::{select, Either};
 use embassy_nrf::gpio::Output;
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::channel::Channel;
+use embassy_sync::pubsub::{PubSubChannel, Subscriber, WaitResult};
 use embassy_time::{Duration, Timer};
+
+#[allow(unused)]
+use {defmt_rtt as _, panic_probe as _};
+
+#[derive(Debug, Clone)]
+pub enum OutputM {
+    T,
+    M,
+    H,
+}
+
+#[derive(Debug, Clone)]
+pub enum InputM {
+    H(usize),
+}
+
+/// inputs to the T
+pub type InputBus = PubSubChannel<ThreadModeRawMutex, InputM, 4, 4, 1>;
+pub type InputSub<'a> = Subscriber<'a, ThreadModeRawMutex, InputM, 4, 4, 1>;
+
+/// outputs from the T
+pub type OutputChannel = Channel<ThreadModeRawMutex, OutputM, 6>;
 
 pub struct T<'a, P: embassy_nrf::gpio::Pin> {
     id: &'a str,
     p: Output<'a, P>,
+    i: InputSub<'a>,
+    o: &'a OutputChannel,
 }
 
 impl<'a, P: embassy_nrf::gpio::Pin> T<'a, P> {
-    pub fn new(id: &'a str, p: Output<'a, P>) -> Self {
-        Self { id, p }
+    pub fn new(id: &'a str, i: InputSub<'a>, o: &'a OutputChannel, p: Output<'a, P>) -> Self {
+        Self { id, p, i, o }
     }
 
     pub fn activate(&mut self) {
@@ -26,7 +53,22 @@ impl<'a, P: embassy_nrf::gpio::Pin> T<'a, P> {
     pub async fn activate_for(&mut self, secs: u64) {
         debug!("activating {} for {}s", self.id, secs);
         self.activate();
-        Timer::after(Duration::from_secs(secs)).await;
+        let x = select(
+            self.i.next_message(),
+            Timer::after(Duration::from_secs(secs)),
+        )
+        .await;
+        match x {
+            Either::First(WaitResult::Message(m)) => match m {
+                InputM::H(_) => {
+                    self.o.send(OutputM::H).await;
+                }
+            },
+            Either::Second(_timeout) => {
+                self.o.send(OutputM::T).await;
+            }
+            _ => debug!("<><><> lag event <><><>"),
+        }
         self.deactivate();
         debug!("deactivated {}", self.id);
     }
